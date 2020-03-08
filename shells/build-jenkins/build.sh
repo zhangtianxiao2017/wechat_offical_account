aaecb5915def2a66e6fa2a596bc4d3e3fae2f332
张天啸 <zhangtianxiao@vip.henu.edu.cn>
2020-03-08 10:20:27 +0800

    'jenkins部署脚本修改'

--- shells/build-jenkins/build.sh
+++ shells/build-jenkins/build.sh
@@ -1,1 +1,15 @@
+
 mvn clean package
+
+dockerName = "wechat_public_access"
+dockerPort = "9000"
+dockerJarVolumn = /home/ubuntu/.jenkins/workspace/wechat_public_access/target/public_access-1.0.jar
+# 停止并删除现有容器
+
+echo "${dockerName}:容器开始停止"
+sudo docker stop ${dockerName}
+sudo docker rm ${dockerName}
+echo "${dockerName}:容器已停止"
+
+echo "${dockerName}:重新启动容器"
+sudo docker run --name={dockerName} -d -p ${dockerPort}:${dockerPort} -v ${dockerJarVolumnggit}:/jar/app.jar ztx/opendjdk:1.0.0
