8f0a3397bb5d107e239e0fe4f1f2f4c79528e709
张天啸 <zhangtianxiao@vip.henu.edu.cn>
2020-03-08 10:55:15 +0800

    '修改jenkins脚本'

--- shells/build-jenkins/build.sh
+++ shells/build-jenkins/build.sh
@@ -12,4 +12,5 @@ sudo docker rm ${dockerName}
 echo "${dockerName}:容器已停止"
 
 echo "${dockerName}:重新启动容器"
-sudo docker run --name=${dockerName} -d -p ${dockerPort}:${dockerPort} -v ${dockerJarVolumnggit}:/jar/app.jar ztx/openjdk:1.0.0
+# sudo docker run --name test -d -p 9000:8080 -v /home/ubuntu/.jenkins/workspace/wechat_public_access/target/public_access-1.0.jar:/jar/app.jar ztx/openjdk:1.0.0
+sudo docker run --name ${dockerName} -d -p ${dockerPort}:8080 -v ${dockerJarVolumnggit}:/jar/app.jar ztx/openjdk:1.0.0
